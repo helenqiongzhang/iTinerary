64382958ace5aaf6b5a5815e27dc2a05ff991611
Helen Qiong Zhang <helenqiongzhang@gmail.com>
2019-09-28 16:34:57 -0400

    tested geo map

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,6 +1,30 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet, Button, ImageBackground } from 'react-native';
+import {
+  Text,
+  View,
+  StyleSheet,
+  Button,
+  ImageBackground,
+  TouchableOpacity,
+} from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
+import Geocoder from 'react-native-geocoding';
+
+Geocoder.init('AIzaSyB41fmo3vZLDtQZbyOv9Uq4fTnvLy0mPf8');
+
+Geocoder.from('NY')
+  .then(json => {
+    var location = json.results[0].geometry.location;
+    console.log('🥳🥳🥳🥳🥳🥳', location);
+  })
+  .catch(error => console.warn(error));
+
+Geocoder.from(41.89, 12.49)
+  .then(json => {
+    var addressComponent = json.results[0].address_components[0];
+    console.log(addressComponent);
+  })
+  .catch(error => console.warn(error));
 
 export default class MapScreen extends Component {
   constructor() {
@@ -10,6 +34,17 @@ export default class MapScreen extends Component {
       longitude: 0,
       error: null,
     };
+  }
+
+  getData() {
+    Geocoder.setApiKey('AIzaSyB41fmo3vZLDtQZbyOv9Uq4fTnvLy0mPf8');
+
+    Geocoder.from('Colosseum')
+      .then(json => {
+        var location = json.results[0].geometry.location;
+        console.log('🥳🥳🥳🥳🥳🥳', location);
+      })
+      .catch(error => console.warn(error));
   }
 
   static navigationOptions = {
@@ -33,6 +68,7 @@ export default class MapScreen extends Component {
   render() {
     return (
       <View style={styles.container}>
+        {/* <View style={styles.container}> */}
         <MapView
           style={styles.map}
           region={{
@@ -44,6 +80,13 @@ export default class MapScreen extends Component {
         >
           <Marker coordinate={this.state} />
         </MapView>
+        <TouchableOpacity
+          onPress={() => {
+            this.getData();
+          }}
+        >
+          <Text>Get Address</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -52,8 +95,8 @@ export default class MapScreen extends Component {
 const styles = StyleSheet.create({
   container: {
     ...StyleSheet.absoluteFillObject,
-    // height: 400,
-    // width: 400,
+    height: 400,
+    width: 400,
     justifyContent: 'flex-end',
     alignItems: 'center',
   },
